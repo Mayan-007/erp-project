79ba09145c02d5a6144181079c4ecd9b66fb6511
Dhruv8803 <92586331+Dhruv8803@users.noreply.github.com>
2022-09-28 15:32:20 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoURI = "mongodb://localhost:27017/shoeRP";
-
+//db connect
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
         console.log("Connected to Mongo Successfully");
