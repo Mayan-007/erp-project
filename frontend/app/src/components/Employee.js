94767149411640d9ef0b5a19999c8d35f848be0e
Kalpit Shah <72689203+kalpitshah2311@users.noreply.github.com>
2022-10-03 19:34:31 +0530

    Update Employee.js

--- frontend/app/src/components/Employee.js
+++ frontend/app/src/components/Employee.js
@@ -1,48 +1,5 @@
 import React from 'react'
 
-
-export default class Employee extends Component {
-    render() {
-        return (
-            <div>
-                <div className="container">
-                    <h2 style={{ marginTop: 10 }}><center>Employee Details</center></h2>
-                    <br></br>
-                    <div className='card'>
-
-                        <div className="card-body">
-                            <form>
-                                <div className='row'>
-                                    <div className='col-md-6'>
-                                        <div className="mb-1">
-                                            <label htmlFor='employee_name' className="form-label">Name</label>
-                                            <input type="text" name='employee_name' id='employee_name' className="form-control"></input>
-                                        </div>
-                                        
-                                        <div className="mb-1">
-                                            <label htmlFor='employee_address' className="form-label">Address</label>
-                                            <input type="text-area" name='employee_address' id='employee_address' className="form-control"></input>
-                                        </div>
-
-                                        <div className="mb-1">
-                                            <label htmlFor='employee_phone' className="form-label">Phone</label>
-                                            <input type="text" name='employee_phone' id='employee_phone' className="form-control"></input>
-                                        </div>
-                                    </div>
-                                    <div className='col-6'>
-                                        <div className="mb-1">
-                                            <label htmlFor='employee_joindate' className="form-label">Join Date</label>
-                                            <input type="date" name='employee_joindate' id='employee_joindate' className="form-control"></input>
-                                        </div>
-                                        <div className="mb-1">
-                                            <label htmlFor='employee_salary' className="form-label">Salary</label>
-                                            <input type="text" name='employee_salary' id='employee_salary' className="form-control"></input>
-                                        </div>
-                                        <div className='mb-1 justify-content-end'>
-                                            <button type="submit" className="btn btn-primary" style={{ marginTop: 30 }}>Submit</button>&nbsp;&nbsp;&nbsp;
-                                            <button type="reset" className="btn btn-primary" style={{ marginTop: 30 }}>Cancel</button>
-                                        </div>
-
 const Employee = () => {
     return (
         <div>
