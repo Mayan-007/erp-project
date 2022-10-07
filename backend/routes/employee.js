e16f6d16dea4d19fb79f45dc0e97168e6d38bcd5
Mayan Prajapati <mayanprajapati007@gmail.com>
2022-10-07 21:16:43 +0530

    Some changes from git bach

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -15,7 +15,7 @@ router.post('/addemployee', [
 ], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        return res.status(200).json({ status: "danger", errors: errors.array() });
     }
 
     try{
