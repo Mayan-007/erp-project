be3c452f97b67c727afcd25cb319e7340fe399fc
Mayan Prajapati <mayanprajapati007@gmail.com>
2022-10-12 10:21:06 +0530

    Customer and Sales
    Both page ready with API Integration

--- frontend/app/src/components/CustomerDetails.js
+++ frontend/app/src/components/CustomerDetails.js
@@ -1,40 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DataTable from 'react-data-table-component';
 
 const CustomerDetails = () => {
+
+    const [customers, setCustomers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/api/customer/getallcustomer')
+            .then(response => response.json())
+            .then(data => setCustomers(data));
+    }, []);
+
+    const columns = [
+        {
+            name: 'Name',
+            selector: row => row.name,
+        },
+        {
+            name: 'Mobile No',
+            selector: row => row.mobile,
+        }
+    ];
+
+    const [search, setSearch] = useState('');
+
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+    }
+
+    const filteredCustomers = customers.filter(customer => {
+        return customer.name.toLowerCase().includes(search.toLowerCase());
+    })
+
     return (
         <div>
             <div className="container">
                 <h2 style={{ marginTop: 10 }}><center> Customer details</center></h2>
                 <div className='card'>
-                    <div className='card-header'>
+                    <div className='card-header bg-secondary'>
                         <div className='row'>
                             <div className='col-4 offset-8'>
-                                <input type="text" className="form-control" placeholder="Search" />
+                                <input type="text" className="form-control" placeholder="Search by customer name" onChange={handleSearch} />
                             </div>
                         </div>
                     </div>
-                    <div className="card-body" style={{ height:'500px', overflowY: 'scroll' }} >
-                        {/* <DataTable
-                            columns={columns}
-                            data={filteredWholesaler}
-                            highlightOnHover
-                            pagination
-                            pointerOnHover
-                        /> */}
-                        <table className='table'>
-                            <thead>
-                                <tr>
-                                 
-                                    <th scope="col-md-4">Customer name</th>
-                                    <th scope="col-md-4" style={{marginRight:'80%'}}>Mobile</th>
-                                 
-                                </tr>
-                              
-                            </thead>
-                            <tbody>
-
-                            </tbody>
-                        </table>
+                    <div className="card-body" style={{ height: '500px', overflowY: 'scroll' }} >
+                        <div className='row d-flex justify-content-center'>
+                            <div className='col-10'>
+                                <DataTable
+                                    columns={columns}
+                                    data={filteredCustomers}
+                                    highlightOnHover
+                                    pagination
+                                    pointerOnHover
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
